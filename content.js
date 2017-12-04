f1d590c39da3ab740a245db68f7783ff5b7b6b02
Ryan F. Salerno <rfsalerno@gmail.com>
2017-12-04 16:38:35 -0500

    Add latest version (worst commit msg - sorry ryan)

--- content.js
+++ content.js
@@ -1,17 +1,63 @@
-InboxSDK.load('1', 'Hello World!').then(function(sdk){
+InboxSDK.load('1', 'sdk_process_c2e2d66213').then(sdk =>{
 
-	// the SDK has been loaded, now do something with it!
-	sdk.Compose.registerComposeViewHandler(function(composeView){
+  const composeViews = new Set();
+  init();
 
-		// a compose view has come into existence, do something with it!
-		composeView.addButton({
-			title: "My Nifty Button!",
-			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
-			onClick: function(event) {
-				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-			},
-		});
+  function garbageCollectEmailComposeViews() {
+    sdk.Compose.registerComposeViewHandler(composeView => {
+      composeViews.add(composeView);
+      composeView.on('destroy', () => {
+        composeViews.delete(composeView);
+      });
+    });
+  }
 
-	});
+  function addCSStoPage() {
+    var cssFile = chrome.extension.getURL('styles.css');
+    var css = $('<link rel="stylesheet" type="text/css">');
+    css.attr('href', cssFile);
+    $('head').first().append(css);
+  }
+
+  function addToolBarButton() {
+    sdk.Toolbars.addToolbarButtonForApp({
+      title: 'ProcessPure',
+      iconUrl: 'http://processpure.co/wp-content/uploads/2017/11/processpure.png',
+      onClick: function (event) {
+        event.dropdown.el.innerHTML = '<p><a class="process-pure-support-link" href="https://app.processpure.co" target="_blank">💰 My Opportunites</a></p><p class="process-pure-support-link"><a id="pp-support">📧 Support & Feedback</a></p>';
+        addClickHandlerToSupportButton();
+      }
+    });
+  }
+
+  function addClickHandlerToSupportButton() {
+    document.getElementById('pp-support').addEventListener('click', event => {
+      sdk.Compose.openNewComposeView().then(() => {
+        const composeView = getActiveComposeView();
+        if (!composeView) { return; }
+        composeView.setSubject('Help me!');
+        composeView.setToRecipients(['ryan@processpure.co']);
+      });
+    });
+  }
+
+  function getActiveComposeView() {
+    // thank you Chris Cowan! https://groups.google.com/forum/#!topic/inboxsdk/4CmyKsstGzU
+    const nonMinimizedComposeViews = Array.from(composeViews).filter(composeView => !composeView.isMinimized());
+    if (document.activeElement) {
+      for (let composeView of nonMinimizedComposeViews) {
+        if (composeView.getBodyElement().contains(document.activeElement)) {
+          return composeView;
+        }
+      }
+    }
+    return nonMinimizedComposeViews[0];
+  }
+
+  function init() {
+    addCSStoPage();
+    addToolBarButton();
+    garbageCollectEmailComposeViews();
+  }
 
 });
